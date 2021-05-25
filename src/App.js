8173e28222b2ffb4d20d65beea4bcac2e8ae5231
jinook929 <jinook929@gmail.com>
2021-05-25 17:00:10 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <CreateTodo />
+        <CreateTodo addTodo={this.addTodo} />
       </div>
     );
   }
